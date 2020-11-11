c0c0ab50a82a7d3714fa88a288098a4f65c14e5c
jknike <61454002+jknike@users.noreply.github.com>
2020-11-12 00:24:40 +0100

    LoginView - demo

---
+++ rejestrator/rejestrator/Viewmodels/BaseViewModel/RelayCommand.cs
@@ -0,0 +1,42 @@
+﻿namespace rejestrator.Viewmodels.BaseViewModel
+{
+    using System;
+    using System.Windows.Input;
+    class RelayCommand : ICommand
+    {
+        readonly Action<object> _execute;
+
+        readonly Predicate<object> _canExecute;
+
+        public RelayCommand(Action<object> execute, Predicate<object> canExecute)
+        {
+            if (execute == null)
+                throw new ArgumentNullException(nameof(execute));
+            else
+                _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null ? true : _canExecute(parameter);
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add
+            {
+                if (_canExecute != null) CommandManager.RequerySuggested += value;
+            }
+            remove
+            {
+                if (_canExecute != null) CommandManager.RequerySuggested -= value;
+            }
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute(parameter);
+        }
+    }
+}
---
+++ rejestrator/rejestrator/Viewmodels/BaseViewModel/ViewModelBase.cs
@@ -0,0 +1,14 @@
+﻿namespace rejestrator.Viewmodels.BaseViewModel
+{
+    using System.ComponentModel;
+    class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(params string[] properties)
+        {
+            if (PropertyChanged != null)
+                foreach (var property in properties)
+                    PropertyChanged(this, new PropertyChangedEventArgs(property));
+        }
+    }
+}
